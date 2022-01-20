990e7b92b4ee2b5e764ce6a2c13240d7cc3c94bc
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-20 09:35:05 +0100

    Finalisation

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -10,8 +10,17 @@ class PageHome extends StatefulWidget {
 class _PageHomeState extends State<PageHome> {
   bool genre = false;
   double? age;
-  double taille = 150;
-  
+  double? taille;
+  double? poids;
+  Map<int, String> activiteSportive = {
+    0:"Faible",
+    1:"Modéré",
+    2:"Forte",
+  };
+  int? activiteSportiveSelection;
+  int? calorieBase;
+  int? calorieAvecActivite;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,9 +63,9 @@ class _PageHomeState extends State<PageHome> {
                           backgroundColor: MaterialStateProperty.all<Color>(getColor())
                       ),
                     ),
-                    TextAvecStyle("Votre taille est de ${taille.toInt()} cm"),
+                    TextAvecStyle("Votre taille est de ${taille?.toInt()} cm"),
                     Slider(
-                        value: taille,
+                        value: taille ?? 100,
                         onChanged: (double d){
                           setState(() {
                             taille = d;
@@ -66,11 +75,22 @@ class _PageHomeState extends State<PageHome> {
                       max: 250,
                       activeColor: getColor(),
                     ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      onChanged: (String value){
+                        setState(() {
+                          poids = double.tryParse(value);
+                        });
+                      },
+                      decoration: const InputDecoration(labelText: "Entrez votre poids en Kilos."),
+                    ),
+                    TextAvecStyle("Quelle est votre activité sportive ?", color: getColor()),
+                    rowActiviteSportive(),
                   ],
                 ),
               ),
               ElevatedButton(
-                onPressed: null,
+                onPressed: calculerNombreDeCalories,
                 child: TextAvecStyle("Calculer", color: Colors.white),
                 style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(getColor())
@@ -121,6 +141,117 @@ class _PageHomeState extends State<PageHome> {
       });
     }
   }
-  
+
+
+  Row rowActiviteSportive(){
+    List<Column> c = [];
+
+    activiteSportive.forEach((key, value) {
+
+      Column colonne = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Radio(
+            value: key,
+            groupValue: activiteSportiveSelection,
+            onChanged: (int? index){
+              setState(() {
+                activiteSportiveSelection = index;
+              });
+            },
+          ),
+          TextAvecStyle(value, color: getColor())
+        ],
+      );
+
+      c.add(colonne);
+    });
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: c,
+    );
+
+  }
+
+
+  void calculerNombreDeCalories(){
+    //Vérification des champs
+    if(age != null && poids != null && activiteSportiveSelection != null && taille != null){
+      if(genre){
+        calorieBase = (66.4730 + (13.7516 * poids!) + (5.0033 * taille!) - (6.7550 * age!)).toInt();
+      }else{
+        calorieBase = (655.0955 + (9.5634 * poids!) + (1.8496 * taille!) - (4.6756 * age!)).toInt();
+      }
+      switch(activiteSportiveSelection){
+        case 0:
+          calorieAvecActivite = (calorieBase! * 1.2).toInt();
+          break;
+        case 1:
+          calorieAvecActivite = (calorieBase! * 1.5).toInt();
+          break;
+        case 2:
+          calorieAvecActivite = (calorieBase! * 1.8).toInt();
+          break;
+        default:
+          calorieAvecActivite = calorieBase;
+          break;
+      }
+      dialogue();
+    }else{
+      //Alerte erreur
+      alerte();
+    }
+  }
+
+
+  Future<void> dialogue() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext contextDialog) {
+          return SimpleDialog(
+            title: TextAvecStyle("Votre besoin en calories", color: getColor()),
+            contentPadding: EdgeInsets.all(5.0),
+            children: [
+              TextAvecStyle("De base : ${calorieBase!}"),
+              TextAvecStyle("Avec Sport : ${calorieAvecActivite!}"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(contextDialog);
+                },
+                child: TextAvecStyle("Ok", color: Colors.white),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        getColor())
+                ),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  Future<void> alerte() async{
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext contextDialog){
+          return AlertDialog(
+            title: Text("Erreur", textScaleFactor: 2,),
+            content: Text("Tous les champs de sont pas remplis"),
+            contentPadding: EdgeInsets.all(5.0),
+            actions: [
+              TextButton(
+                  onPressed: (){
+                    Navigator.pop(contextDialog);
+                  },
+                  child: Text("Ok", style: TextStyle(color: Colors.red),)
+              ),
+            ],
+          );
+        }
+    );
+  }
 
 }
